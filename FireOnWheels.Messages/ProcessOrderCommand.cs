7f393df495df1ed012c6f794160192e4d78dd23d
edgaguil <edgaguil@COBOGPGP1556>
2017-12-26 11:55:01 -0500

    Proyecto de Mensajes agregado a la solucion

---
+++ FireOnWheels.Messages/ProcessOrderCommand.cs
@@ -0,0 +1,17 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FireOnWheels.Messages
+{
+    public class ProcessOrderCommand : ICommand
+    {
+        public string AddressFrom { get; set; }
+        public string AddressTo { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
